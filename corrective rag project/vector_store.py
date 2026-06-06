2f76d29946825b9cbb17fa2ea9455f9a62fa60c1
akshat <akshat@users.noreply.github.com>
2026-06-06 20:52:11 +0530

    Fix vector store text splitter import for newer LangChain

--- corrective rag project/vector_store.py
+++ corrective rag project/vector_store.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 import chromadb
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
